f47cac64c8d2115b184d576f23ca00a409c866da
Joao Moreira <joaomarcello.mm@gmail.com>
2023-02-23 09:50:09 -0300

    adicionei luz

--- scripts/lamp.lua
+++ scripts/lamp.lua
@@ -2,6 +2,9 @@ local Component = require "scripts.component"
 
 local imgs
 
+---@type love.Image|any
+local img_light
+
 ---@class Lamp : GameComponent
 local Lamp = setmetatable({}, Component)
 Lamp.__index = Lamp
@@ -30,6 +33,11 @@ function Lamp:__constructor__(state, wire, args)
     local color = wire.color_hidden
     self.anima_off = _G.JM_Anima:new { img = imgs[color][0] }
     self.anima_on = _G.JM_Anima:new { img = imgs[color][1] }
+
+    self.light = _G.JM_Anima:new { img = img_light }
+    self.light:set_size(45, 45)
+    self.light:set_color2(unpack(color))
+    self.light:set_color2(nil, nil, nil, 0.8)
 end
 
 ---@param wire Game.Component.Wire
@@ -59,9 +67,13 @@ function Lamp:load(wire)
                 [1] = love.graphics.newImage('/data/image/lamp yellow on.png')
             },
         }
+
+    img_light = img_light or love.graphics.newImage('/data/image/light source.png')
 end
 
 function Lamp:finish()
+    local r = img_light and img_light:release()
+    img_light = nil
     imgs = nil
 end
 
@@ -76,6 +88,10 @@ end
 function Lamp:my_draw()
     if self.wire:is_plugged() then
         self.anima_on:draw_rec(self:rect())
+
+        love.graphics.setBlendMode("add")
+        self.light:draw(self.x + self.w / 2, self.y + self.h / 2)
+        love.graphics.setBlendMode("alpha")
     else
         self.anima_off:draw_rec(self:rect())
     end
