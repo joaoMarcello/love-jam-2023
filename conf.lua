50b7cdd9d0ab12ee17557eec7d391c1410bbd3f8
Joao Moreira <joaomarcello.mm@gmail.com>
2023-03-03 18:28:50 -0300

    minor changes

--- conf.lua
+++ conf.lua
@@ -17,7 +17,7 @@ function love.conf(t)
     -- local scale = 2
     -- local width = 320
     -- local height = 160
-    t.window.width = 1366 -- The window width (number)
+    t.window.width = 1024 -- The window width (number)
     t.window.height = 768 -- The window height (number)
     t.window.borderless = false -- Remove all border visuals from the window (boolean)
     t.window.resizable = false -- Let the window be user-resizable (boolean)
